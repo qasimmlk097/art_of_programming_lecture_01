96847fb71b340b71ef9dcd6d79b0462ac6bef4f0
qasimmlk097 <83378352+qasimmlk097@users.noreply.github.com>
2024-02-06 15:33:40 +0500

    Add files via upload

---
+++ Henosis/js/first.js
@@ -0,0 +1,15 @@
+function Qasim(){
+    const emailAdress= document.getElementById('username');
+
+
+
+    if(emailAdress.value){
+        alert('there is value');
+        alert('there is a value');
+    
+    }
+    else{
+        console.log('there is no value'); 
+        alert('there is no a value');
+    }
+}
